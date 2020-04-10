5d52c79bf6e8425c2967faeea32cf8aba674cc5f
Raphael-AWS <persishep@gmail.com>
2020-04-10 20:20:58 +0000

    come on!

---
+++ src/test/groovy/com/example/shining_stars/DemoApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.example.shining_stars
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
